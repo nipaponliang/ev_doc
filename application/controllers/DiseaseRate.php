8be29306438a249ba299adc7661167a56b1b9dac
adisorn <beckadisorn@kkumail.com>
2021-06-22 11:03:49 +0700

    rename

--- application/controllers/DiseaseRate.php
+++ application/controllers/DiseaseRate.php
@@ -5,25 +5,25 @@ class DiseaseRate extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('template/อัตราการเกิดโรคกับปัจจัยต่างๆ');
+		$this->load->view('template/disease rate');
 	}
     public function get1(){
-        $this->load->view('template/อัตราการเกิดโรคกับที่อยู่อาศัย');
+        $this->load->view('template/disease rate housing');
     }
 	public function get2(){
-        $this->load->view('template/อัตราการเกิดโรคกับอายุ');
+        $this->load->view('template/disease rate age');
     }
 	public function get3(){
-        $this->load->view('template/อัตราการเกิดโรคกับอาชีพ');
+        $this->load->view('template/disease rate occupation');
     }
 	public function get4(){
-        $this->load->view('template/เลือกปัจจัยการวิเคราะห์');
+        $this->load->view('template/analytical factor');
     }
     public function serchProvince(){
-        $this->load->view('template/อัตราการเกิดโรคกับที่อยู่อาศัยแสดง');
+        $this->load->view('template/disease rate housing output');
     }
     public function get4Show(){
-        $this->load->view('template/เลือกปัจจัยการวิเคราะห์แสดง');
+        $this->load->view('template/analytical factor output');
     }
     public function delete(){
         echo "delete Member";
