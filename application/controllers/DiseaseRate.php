ef0dcc907b82d333b9918476f7c41d02e56aa80e
siriyakorndo <76682951+siriyakorndo@users.noreply.github.com>
2021-06-29 17:24:55 +0700

    Update DiseaseRate.php

--- application/controllers/DiseaseRate.php
+++ application/controllers/DiseaseRate.php
@@ -91,4 +91,13 @@ class DiseaseRate extends CI_Controller {
 	// 	$this->load->view('nav');
 	// 	$this->load->view('treatment_history',$data);
 	// }
+	
+	public function treatment_history()
+	{
+		$q  = "SELECT * FROM meeting_detail INNER JOIN doctor ON doctor.ID_doc = meeting_detail.ID_doc INNER JOIN patient ON patient.ID = meeting_detail.ID";
+        	$data['rows'] = $this->db->query($q)->result_object();
+		$this->load->view('nav');
+		$this->load->view('treatment_history',$data);
+	}
+
 }
