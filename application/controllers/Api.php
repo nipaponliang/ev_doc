da717dff3d760429883c09bc54dc03f31b9faf03
siriyakorndo <76682951+siriyakorndo@users.noreply.github.com>
2021-06-29 17:23:15 +0700

    Update Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -28,6 +28,8 @@ class Api extends CI_Controller {
         if (mysqli_connect_error($connect)) {
             echo 'failed to connect';
         }
+	    
+	
 
         $causeCancel = $_POST['causeCancel'];
         $causeCancel = $_POST['nameDoc'];
@@ -36,5 +38,26 @@ class Api extends CI_Controller {
         mysqli_query($connect,"INSERT INTO causecancel (causeCancel,nameDoc,detail,dating)
                                 VALUES ('$causeCancel','$nameDoc','$detail','$dating')");
     } 
+	
+	public function get_data()
+	{
+        $id = $this->input->post("id");
+		$q = "SELECT * FROM meeting_detail 
+        INNER JOIN doctor ON doctor.ID_doc = meeting_detail.ID_doc 
+        INNER JOIN patient ON patient.ID = meeting_detail.ID 
+        WHERE ID_meeting=".$id;
+        $row = $this->db->query($q)->row();
+        echo json_encode(array("flag"=>1,"data"=>$row));
+	}
+
+    function save_data_dc(){
+        $id = $this->input->post("id");
+        // $rate_1 = $this->input->post("rate_1");
+        // $rate_2 = $this->input->post("rate_2");
+
+		$q = "UPDATE meeting_detail SET rating_doc = 6,
+        rating_cli = 2
+        WHERE meeting_detail.ID_meeting = 2 "; 
+    }
 
 }
