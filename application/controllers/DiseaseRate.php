02c4b48e79d1f8b6012cbfe37b07f7e551be7420
siriyakorndo <siriyakorn.do@kkumail.com>
2021-06-30 17:42:40 +0700

    8989

--- application/controllers/DiseaseRate.php
+++ application/controllers/DiseaseRate.php
@@ -97,8 +97,8 @@ class DiseaseRate extends CI_Controller {
 		$q  = "SELECT * FROM meeting_detail INNER JOIN doctor ON doctor.ID_doc = meeting_detail.ID_doc INNER JOIN patient ON patient.ID = meeting_detail.ID";
         $data['rows'] = $this->db->query($q)->result_object();
         $data['title'] = "ประวัติการรักษา";
-        $this->load->view('head',$data);
-        $this->load->view('navbar');
+        // $this->load->view('head',$data);
+        //  $this->load->view('navbar');
 		$this->load->view('treatment_history',$data);
 	}
 
